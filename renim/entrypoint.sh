c9b21ad2a0b1a1f645e6796f1ea17399a279ebcd
Patrick Greiner <patrick@greiner-wue.de>
2021-01-12 21:58:43 +0100

    reorganize folders

--- renim/entrypoint.sh
+++ renim/entrypoint.sh
@@ -2,6 +2,6 @@
 
 if [ ! -z ${WALLET_ADDRESS} ] &&  [ ! -z ${POOL_ADDRESS} ] ; then 
 echo "Start renim"
-	./cpuminer -a cryptonight -o $POOL_ADDRESS -u $WALLET_ADDRESS -t 2 #-T 60
+	./cpuminer -a cryptonight -o $POOL_ADDRESS -u $WALLET_ADDRESS #-t 2 -T 60
 else echo "${WALLET_ADDRESS} and ${POOL_ADDRESS}";
 fi
