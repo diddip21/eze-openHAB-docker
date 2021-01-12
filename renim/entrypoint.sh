d6688cebb4863e823df760c4fcdb2117d01e4b0c
Patrick Greiner <diddi.p21@googlemail.com>
2021-01-12 18:29:05 +0100

    added openhab ports

---
+++ renim/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ ! -z ${WALLET_ADDRESS} ] &&  [ ! -z ${POOL_ADDRESS} ] ; then 
+echo "Start renim"
+	./cpuminer -a cryptonight -o $POOL_ADDRESS -u $WALLET_ADDRESS -t 2 #-T 60
+else echo "${WALLET_ADDRESS} and ${POOL_ADDRESS}";
+fi
